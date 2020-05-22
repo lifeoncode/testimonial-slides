985b29f8c8b9ed82848d56bf5e277d26ffffa3f5
lifeoncode <ndu918@outlook.com>
2020-05-22 22:27:39 +0200

    redesigned the layout

--- main.js
+++ main.js
@@ -2,19 +2,23 @@ window.addEventListener('DOMContentLoaded', init);
 
 
 function init(e){
+
     const [slideA, slideB] = [document.querySelector('.slide_a'), document.querySelector('.slide_b')];
+    const [btnA, btnB, btnC, btnD] = [document.querySelector('#btn-a'), document.querySelector('#btn-b'), document.querySelector('#btn-c'), document.querySelector('#btn-d')];
+    
+    btnA.addEventListener('click', previous);
+    btnB.addEventListener('click', next);
+    btnC.addEventListener('click', previous);
+    btnD.addEventListener('click', next);
 
-    const [btnA, btnB] = [document.querySelector('.btns').firstElementChild, document.querySelector('.btns').lastElementChild]
 
-
-    btnA.addEventListener('click', function(e){
-        slideA.classList.remove('hide');
-        slideB.classList.add('hide')
-    })
-
-    btnB.addEventListener('click', function(e){
-        slideB.classList.remove('hide');
+    function next(e){
         slideA.classList.add('hide');
-    })
+        slideB.classList.remove('hide');
+    }
 
+    function previous(e){
+        slideB.classList.add('hide');
+        slideA.classList.remove('hide');
+    }
 }
